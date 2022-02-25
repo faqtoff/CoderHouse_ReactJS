eb4aa25fedd5fdffc806cf2804992a0027a6ff3c
Facundo Toffolo Pasquini <f.toff.pas@gmail.com>
2022-02-24 21:43:41 -0300

    Agrego archivos scss y componente NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+export const NavBar = () => {
+  const [show, setShow] = useState('')
+  return (
+    <nav className="navbar--faqstyle bg--primary">
+        <label className='navbar__brand'>
+            <a className="navbar__link" rel="folow" href="/">Tienda</a>
+        </label>
+        <ul className={`navbar__linkGroup ${show}`}>
+            <li>
+                <a className="navbar__link" rel="folow" href='/qr-code'>Inicio</a>
+            </li>
+            <li>
+                <a className="navbar__link" rel="folow" href='/tienda'>Productos</a>
+            </li>
+            <li>
+                <a className="navbar__link" rel="folow" href='/contacto'>Contacto</a>
+            </li>
+        </ul>
+        <span className='navbar__btn' onClick={() => show===''?setShow('show'):setShow('')}>
+            <i className="fas fa-bars"></i>
+        </span>
+    </nav>
+  )
+}
